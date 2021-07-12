bef90bda56ae780dca404ece6e96a2131a54adb9
Pedro Trujillo <ptrujillo@velocityz.com>
2021-07-12 16:34:23 -0400

    Added project to repo

---
+++ Rick&MortyResidents/DataSource/DataSource.swift
@@ -0,0 +1,8 @@
+//
+//  DataSource.swift
+//  Rick&MortyResidents
+//
+//  Created by Pedro Enrique Trujillo Vargas on 7/12/21.
+//
+
+import Foundation
---
+++ Rick&MortyResidents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&MortyResidents
+//
+//  Created by Pedro Enrique Trujillo Vargas on 7/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
